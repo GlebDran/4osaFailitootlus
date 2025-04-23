82bdd370f36e39cac8a0b792ff8211b43a1b1647
opilane <opilane@HP-CZC2349HT0>
2025-04-23 15:54:24 +0300

    upd

--- 5. osa C# Kollektsioonid. Listid ja sõnastikud. Meetodid ja näidised/MainClass5osa.cs
+++ 5. osa C# Kollektsioonid. Listid ja sõnastikud. Meetodid ja näidised/MainClass5osa.cs
@@ -1,12 +1,104 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace _4osaFailitootlus._5._osa_C__Kollektsioonid._Listid_ja_sõnastikud._Meetodid_ja_näidised
 {
-    class MainClass5osa
+    internal class MainClass_5osa
     {
+        public static void Main(string[] args)
+        {
+            ArrayList nimed = new ArrayList();
+            nimed.Add("Kati");
+            nimed.Add("Mati");
+            nimed.Add("Juku");
+
+            if (nimed.Contains("Mati"))
+                Console.WriteLine("Mati olemas");
+
+            Console.WriteLine("Nimesid kokku: " + nimed.Count);
+
+            nimed.Insert(1, "Sass");
+
+            Console.WriteLine("Mati indeks: " + nimed.IndexOf("Mati"));
+            Console.WriteLine("Mari indeks: " + nimed.IndexOf("Mari"));
+
+            foreach (string nimi in nimed)
+                Console.WriteLine(nimi);
+
+            Console.ReadLine();
+
+
+
+            Tuple<float, char> route = new Tuple<float, char>(2.5f, 'N');
+            Console.WriteLine($"Vahemaa: {route.Item1}, Suund: {route.Item2}");
+
+            Console.ReadLine();
+
+            List<Person> people = new List<Person>();
+            people.Add(new Person() { Name = "Kadi" });
+            people.Add(new Person() { Name = "Mirje" });
+
+            foreach (Person p in people)
+                Console.WriteLine(p.Name);
+
+            Console.ReadLine();
+
+
+
+            LinkedList<int> loetelu = new LinkedList<int>();
+            loetelu.AddLast(5);
+            loetelu.AddLast(3);
+            loetelu.AddFirst(0);
+
+            foreach (int arv in loetelu)
+                Console.WriteLine(arv);
+
+            loetelu.RemoveFirst();
+            loetelu.RemoveLast();
+            loetelu.AddLast(555);
+            loetelu.Remove(555);
+
+            Console.ReadLine();
+
+
+
+            Dictionary<int, string> riigid = new Dictionary<int, string>();
+            riigid.Add(1, "Hiina");
+            riigid.Add(2, "Eesti");
+            riigid.Add(3, "Itaalia");
+
+            foreach (var paar in riigid)
+                Console.WriteLine($"{paar.Key} - {paar.Value}");
+
+            string pealinn = riigid[2];
+            riigid[2] = "Eestimaa";
+            riigid.Remove(3);
+
+            Console.ReadLine();
+
+
+            Dictionary<char, Person> inimesed = new Dictionary<char, Person>();
+            inimesed.Add('k', new Person() { Name = "Kadi" });
+            inimesed.Add('m', new Person() { Name = "Mait" });
+
+            foreach (var entry in inimesed)
+                Console.WriteLine($"{entry.Key} - {entry.Value.Name}");
+
+            Console.ReadLine();
+
+
+
+        }
+
+        class Person
+        {
+            public string Name { get; set; }
+        }
+
     }
 }
